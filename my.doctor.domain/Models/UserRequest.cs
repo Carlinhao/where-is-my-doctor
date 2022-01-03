0f3b2a547eb4e80e3c3a0f03662907410a3e519e
Carlinhao <carsilva.ti@gmail.com>
2022-01-03 17:51:14 -0300

    Add new properties in class UserRequest

--- my.doctor.domain/Models/UserRequest.cs
+++ my.doctor.domain/Models/UserRequest.cs
@@ -2,6 +2,8 @@
 {
     public class UserRequest
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
     }
