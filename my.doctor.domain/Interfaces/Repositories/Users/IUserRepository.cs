2a218bf1f9eee001488f1f6080f046f6de6e0208
Carlinhao <carsilva.ti@gmail.com>
2022-01-03 17:50:46 -0300

    Create interface IUserRepository

---
+++ my.doctor.domain/Interfaces/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using my.doctor.domain.Models;
+using System.Security.Cryptography;
+
+namespace my.doctor.domain.Interfaces.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        Task<UserRequest> CanDoLogin(UserRequest request);
+        Task RegisterUser(UserRequest request);
+        Task<string> ComputeHash(string input, SHA256CryptoServiceProvider algorithm);
+    }
+}
