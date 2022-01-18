5e55ce977564f8e5261e1c79d58bfa2a933cec85
Carlinhao <carsilva.ti@gmail.com>
2022-01-18 00:20:20 -0300

    Inherits class base to login

--- my.doctor.domain/ViewModels/UserViewModel.cs
+++ my.doctor.domain/ViewModels/UserViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace my.doctor.domain.ViewModels
 {
-    public class UserViewModel
+    public class UserViewModel : BaseLoginViewModel
     {
         [Required]
         [MinLength(2, ErrorMessage = "O nome deve conter mais de 2 caracteres.")]
@@ -10,16 +10,6 @@ namespace my.doctor.domain.ViewModels
 
         [EmailAddress]
         [MinLength(250, ErrorMessage = "Informe um email válido")]
-        public string Email { get; set; }
-
-        [Required]
-        [MinLength(2, ErrorMessage = "O nome deve conter mais de 2 caracteres.")]
-        [MaxLength(7, ErrorMessage = "O número máximo de caracteres para o Login é 7!")]
-        public string Login { get; set; }
-
-        [Required]
-        [MinLength(2, ErrorMessage = "A senha deve conter no mínimo 2 caracteres.")]
-        [MaxLength(50, ErrorMessage = "A senha deve conter no máximo 50 caracteres.")]
-        public string Password { get; set; }
+        public string Email { get; set; }        
     }
 }
